1fde1566ac469cb04dcd068edeb3575983b35cfd
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-01-19 18:57:12 +0200

    Completed second activity by sending email

--- app/src/main/java/com/example/gitrootdreamslessonshometasks/MainActivity.kt
+++ app/src/main/java/com/example/gitrootdreamslessonshometasks/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -32,7 +34,24 @@ class MainActivity : AppCompatActivity() {
         val buttonEmail: Button = findViewById(R.id.sendEmailButton)
         buttonEmail.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+            intent.putExtra(MESSAGE_TO_SECOND_ACTIVITY, "Message from first activity")
+            /*val bundle = Bundle()
+            bundle.putParcelable(PARCELABLE_MESSAGE, CustomMessage("custom message"))*/
+            startActivityForResult(intent, 600)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if(requestCode == 600 && resultCode == RESULT_OK){
+            val message = intent?.getStringExtra(SecondActivity.MESSAGE_TO_FIRST_ACTIVITY)
+            message?.let {
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    companion object{
+        const val MESSAGE_TO_SECOND_ACTIVITY = "MessageToSecondActivity"
+        const val PARCELABLE_MESSAGE = "ParcelableMessage"
     }
 }

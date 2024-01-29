f9c2a1c4c506faa11178917651431a212bb38186
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-01-29 07:02:32 +0200

    Corrected layout with weather forecast

--- app/src/main/java/com/example/gitrootdreamslessonshometasks/MainActivity.kt
+++ app/src/main/java/com/example/gitrootdreamslessonshometasks/MainActivity.kt
@@ -12,25 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.biography)
-        textView.setText("  I was born on 12.19.1963 in the city of Khorostkiv, Ternopil region. " +
-                " Since I graduated from Lviv State University in 1985, I have been working and living in Lviv (the best city of Ukraine).\n" +
-                "  My experience as a C/C++ developer and electronics engineer is more 36 years. "  +
-                "I started my professional work as a student (1983–1985. University research sector as a Laboratory assistant. Repairing and maintenance of electronic equipment. Software development.). " +
-                "Then I continued a long professional path as an engineer and programmer:\n" +
-                " 1985-2003 - R&D Division in Scientific Research Institute of PHTE Lviv\n" +
-                " 2005-2011 - PDT Ukraine\n" +
-                " 2012-2017 - Cypress Microsystems\n" +
-                " 2018-2019 - Ezlo Innovation\n"+
-                " 2021-2023 - Intellias.\n" +
-                "  However, it's never too late to learn - now I aspire to become an Android developer as well." +
-                " I want to start my own business, combined with Home Automation, to pass on to my nephew and my grandson.")
+        val textView = findViewById<TextView>(R.id.myPhone)
+        textView.setText("  My phone number is +38-067-681-76-51")
 
-        val buttonPhone: Button = findViewById(R.id.callPhoneButton)
-        buttonPhone.setOnClickListener {
-            val toast = Toast.makeText(this, "My phone number: +38067-681-7651", Toast.LENGTH_LONG)
-            toast.show()
-        }
+//        val buttonPhone: Button = findViewById(R.id.callPhoneButton)
+//        buttonPhone.setOnClickListener {
+//            val toast = Toast.makeText(this, "My phone number: +38067-681-7651", Toast.LENGTH_LONG)
+//            toast.show()
+//        }
 
         val buttonEmail: Button = findViewById(R.id.sendEmailButton)
         buttonEmail.setOnClickListener {

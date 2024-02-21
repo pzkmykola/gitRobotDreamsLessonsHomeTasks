8496b7a192416ace0ad8253574852b0489f2188d
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-02-21 08:40:31 +0200

    added dependencies for dagger-hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -27,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     viewBinding { enable=true }
 }
@@ -52,7 +54,17 @@ dependencies {
     implementation ("com.squareup.okhttp3:okhttp:4.11.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 
+    //hilt
+//    implementation ("com.google.dagger:hilt-android:2.46.1")
+//    kapt ("com.google.dagger:hilt-compiler:2.46.1")
+    implementation ("com.google.dagger:hilt-android:2.50")
+    kapt ("com.google.dagger:hilt-compiler:2.50")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt{
+    correctErrorTypes = true
 }
--- app/src/main/java/com/example/gitrootdreamslessonshometasks/ApiClient.kt
+++ app/src/main/java/com/example/gitrootdreamslessonshometasks/ApiClient.kt
@@ -1,15 +1,13 @@
 package com.example.gitrootdreamslessonshometasks
 
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-//class ApiClient {
-//    companion object {
-//        val client = Retrofit.Builder()
-//            .baseUrl("https://goweather.herokuapp.com")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//    }
-//}
+class ApiClient {
+    companion object {
+        val client = Retrofit.Builder()
+            .baseUrl("https://goweather.herokuapp.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}

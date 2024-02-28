3d3441b6282ef599f45d089f8b88e0a83803a524
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-02-28 19:29:48 +0200

    added dynamic animation for buttons

--- app/src/main/java/com/example/gitrootdreamslessonshometasks/MainActivity.kt
+++ app/src/main/java/com/example/gitrootdreamslessonshometasks/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.gitrootdreamslessonshometasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import androidx.constraintlayout.widget.Guideline
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import androidx.fragment.app.FragmentManager
 import com.example.gitrootdreamslessonshometasks.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
@@ -21,10 +23,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val firstFragment = FirstFragment()
         manager = supportFragmentManager
         manager.beginTransaction()
-            .add(com.google.android.material.R.id.container, FirstFragment() )
+            .add(com.google.android.material.R.id.container, firstFragment)
             .commit()
+
         GlobalScope.launch(Dispatchers.Main) {
             val secondFragment = SecondFragment()
             delay(3100)
@@ -33,6 +38,22 @@ class MainActivity : AppCompatActivity() {
                 .setCustomAnimations(R.anim.slide_in_left, R.anim.slide_in_left)
                 .add(com.google.android.material.R.id.container, secondFragment)
                 .commit()
+            val phoneButton = firstFragment.view?.findViewById<Button>(R.id.callPhoneButton)
+            val emailButton = firstFragment.view?.findViewById<Button>(R.id.sendEmailButton)
+
+            delay(1600)
+            val guideLine = firstFragment.view?.findViewById<Guideline>(R.id.guideLine)
+            phoneButton?.visibility = View.VISIBLE
+            emailButton?.visibility = View.VISIBLE
+
+            delay(200)
+            val springAnimation = SpringAnimation(emailButton, DynamicAnimation.X)
+            val springForce = SpringForce()
+            springForce.finalPosition = (guideLine?.pivotY?: 0.0F)/2
+            springForce.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+            springForce.stiffness = SpringForce.STIFFNESS_VERY_LOW
+            springAnimation.spring = springForce
+            springAnimation.start()
         }
     }
 }
